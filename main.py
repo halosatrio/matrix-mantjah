a0c433a32ee5a63add1f052cec92e484dd012467
halosatrio <loredana@disroot.org>
2020-11-29 01:08:42 +0700

    maxdigit decimal cuy

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ import random
 
 num = int(sys.argv[1])
 maxnum = (num*num)
-
 generated = random.sample(range(0, maxnum), maxnum)
+
+maxdigit = len(str(maxnum-1))
+
+decimal = str(5).zfill(maxdigit)
 
 matrix = np.array(generated).reshape((num, num))
 
 print(generated)
 print('')
 print(matrix)
+print('')
+print(maxdigit)
+print('')
+print(decimal)
