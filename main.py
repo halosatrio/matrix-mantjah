d254567e078b79ddfe21ae14fb8347059b472810
halosatrio <loredana@disroot.org>
2020-11-29 00:32:14 +0700

    generate list random number in range(0,n*n)

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import sys
 import numpy as np
+import random
 
 num = int(sys.argv[1])
+maxnum = (num*num)
 
 matrix = np.zeros(shape=(num, num))
+generated = random.sample(range(0, maxnum), maxnum)
 
 print(matrix)
+print(generated)
