40f6daccae56270879c450265e3635fa436d6ff8
halosatrio <loredana@disroot.org>
2020-11-29 00:44:48 +0700

    bikin matrix NumxNum isinya random number(0,maxnum)

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import random
 num = int(sys.argv[1])
 maxnum = (num*num)
 
-matrix = np.zeros(shape=(num, num))
 generated = random.sample(range(0, maxnum), maxnum)
 
-print(matrix)
+matrix = np.array(generated).reshape((num, num))
+
 print(generated)
+print('')
+print(matrix)
